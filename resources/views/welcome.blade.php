0ac7ad9a7079565c117e41d073b01356227469d3
Sven Oliver Berger <bergersvenoliver@gmail.com>
2026-02-21 00:07:04 +0100

    Wuhuu!!!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,13 +13,6 @@
         <h1 class="text-4xl font-bold text-gray-200 mb-6">Tailwind Test</h1>
         <p class="text-lg text-gray-200 mb-8">Laravel + Tailwind lokal & auf Server!</p>
         <p class="text-md text-gray-200 mb-6">Deploy via GitHub Webhook aktiv.</p>
-        <div class="bg-green-100 p-6 rounded-2xl border-4 border-green-400">
-            <p class="text-green-800 font-semibold">✅ Erfolg: Tailwind-Klassen wirken!</p>
-        </div>
-    </div>
-
-    <div class="bg-yellow-100 mb-20 p-5 border-2 border-yellow-400 rounded-lg mt-10 max-w-md w-full mx-4 text-center">
-        <p class="text-black font-semibold">⚠️ Hinweis: Bitte Cache leeren, wenn Änderungen nicht sichtbar sind.</p>   
     </div>
 </body>
 
