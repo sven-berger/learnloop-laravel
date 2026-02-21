450e81642bf7599d3c639ddf28850c5cb9ae028d
Sven Oliver Berger <bergersvenoliver@gmail.com>
2026-02-22 00:12:09 +0100

    move complete static box to component

--- resources/views/layouts/sidebar/ThatsMe.blade.php
+++ resources/views/layouts/sidebar/ThatsMe.blade.php
@@ -4,60 +4,14 @@
 @endphp
 
 <x-box>
-    <x-h2 title="Das bin ich"></x-h2>
-    <div class="block mb-10">
-        <dl class="grid grid-cols-[auto_1fr] gap-x-6 gap-y-2 mb-3">
-            <dt class="text-gray-500">Name:</dt>
-            <dd class="font-medium text-gray-500">Sven Oliver Berger</dd>
-
-            <dt class="text-gray-500">Alter:</dt>
-            <dd class="font-medium text-gray-500">{{ $age }}</dd>
-
-            <dt class="text-gray-500">Wohnort:</dt>
-            <dd class="font-medium text-gray-500">65510 • Idstein</dd>
-
-            <dt class="text-gray-500">E-Mail:</dt>
-            <dd class="font-medium text-gray-500">
-                <span class="text-sm">
-                    <a href="mailto:bergersvenoliver@gmail.com">bergersvenoliver@gmail.com</a>
-                </span>
-            </dd>
-            <dt class="text-gray-500">Handynummer:</dt>
-            <dd class="font-medium text-gray-500">+49 15119409788</dd>
-        </dl>
-
-        <hr class="border-slate-400 my-5" />
-
-        <dl class="grid grid-cols-[auto_1fr] gap-x-6 gap-y-2 mb-3">
-            <dt class="text-gray-500">Betrieb:</dt>
-            <dd class="font-medium text-gray-500">WolkenWerk GmbH</dd>
-
-            <dt class="text-gray-500">Status:</dt>
-            <dd class="font-medium text-gray-500">Praktikant</dd>
-        </dl>
-        <!-- 
-        <hr class="border-slate-400 my-5" />
-
-        <dl class="block grid-cols-[auto_1fr] gap-x-6 gap-y-2 mb-3">
-            <dt class="text-gray-500 mb-5">Über mich:</dt>
-            <dd class="font-medium text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-                alias mollitia nemo soluta, libero tempore veritatis numquam aliquam officiis? Doloribus.</dd>
-        </dl> -->
-    </div>
-
-    <!-- <div class="lg:mb-5 lg:w-96 lg:h-96 md:w-48 h-48 bg-gray-100 lg:flex lg:items-center lg:justify-center rounded-xl">
-        <img src="/images/sidebarImage1.jpg" alt=""
-            class="w-full h-full object-cover object-top rounded-xl transition-transform duration-300 hover:scale-105 my-10" />
-    </div>
-
-    <div class="lg:mb-5 lg:w-96 lg:h-96 md:w-48 h-48 bg-gray-100 lg:flex lg:items-center lg:justify-center rounded-xl">
-        <img src="/images/sidebarImage2.jpg" alt=""
-            class="w-full h-full object-cover object-top rounded-xl transition-transform duration-300 hover:scale-105 my-10" />
-    </div>
-
-    <div class="lg:mb-5 lg:w-96 lg:h-96 md:w-48 h-48 bg-gray-100 lg:flex lg:items-center lg:justify-center rounded-xl">
-        <img src="/images/sidebarImage3.png" alt=""
-            class="w-full h-full object-cover object-top rounded-xl transition-transform duration-300 hover:scale-105 my-10" />
-    </div> -->
-
+    <x-h2 title="Das bin ich" class="mb-8"></x-h2>
+    <x-myprofile name="Sven" age="{{ $age }}" location="Limburger Straße 30 • 65510 Idstein"
+        email="bergersvenoliver@gmail.com" phone="+49 1511 9409788" about="Mein Traumberuf liegt in der Webentwicklung, PHP und
+        JavaScript (HTML und CSS selbstredend)<br /><br />
+    Die Kombination aus Laravel (PHP), Stimulus (JavaScript) und Tailwind CSS (CSS) ist für mich die perfekte Mischung,
+    um moderne und effiziente Webanwendungen zu erstellen.<br /><br />
+    Mit React(Js) habe ich ebenfalls über Monate Erfahrung gesammelt, finde es spannend, aber ich denke mir: Da geht
+    noch Mehr!<br /><br />
+    Ich bin begeistert von der Möglichkeit, kreative Lösungen zu entwickeln und gleichzeitig eine saubere und wartbare
+    Codebasis zu pflegen." company="WolkenWerk GmbH" status="Prakikant" />
 </x-box>
