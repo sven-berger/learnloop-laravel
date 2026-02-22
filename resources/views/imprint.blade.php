51f50827a82c1d1380175634414a0033e80cf92d
Sven Oliver Berger <bergersvenoliver@gmail.com>
2026-02-22 23:39:24 +0100

    add dsgvo content

--- resources/views/imprint.blade.php
+++ resources/views/imprint.blade.php
@@ -1,13 +1,36 @@
 <x-public-layout>
     <x-layout.content>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur reprehenderit unde incidunt voluptatem
-            reiciendis odio voluptas delectus ipsam ea, accusamus, ipsa quam officiis corrupti rem consequatur ad
-            voluptatibus sint dolorem eos? Nulla adipisci quidem aspernatur dolores velit, facere hic! Commodi maxime
-            sunt,
-            earum, nam ex illum, id quis suscipit natus vitae quod dignissimos tempore? Tenetur a porro velit temporibus
-            provident magnam accusamus minus sapiente corporis quibusdam dolor, natus culpa maxime, aliquid similique
-            harum
-            quidem amet reiciendis vel eum. Natus modi voluptatum alias, sit in, animi eum distinctio eveniet veritatis,
-            fugiat delectus laborum nisi perspiciatis voluptatibus nobis consequuntur. Cumque, obcaecati facilis!</p>
+        <x-layout.h2 title="Allgemeine Informationen"></x-layout.h2>
+        <dl class="flex flex-col gap-2">
+            <dt class="text-green-700 font-bold">Vollständiger Name</dt>
+            <dd>Sven Oliver Berger</dd>
+
+            <dt class="text-green-700 font-bold">Adresse</dt>
+            <dd>
+                <p>Limburger Str. 30</p>
+                <p>65510 Idstein</p>
+            </dd>
+
+            <dt class="text-green-700 font-bold">Telefon</dt>
+            <dd>+49 1511 9409788</dd>
+
+            <dt class="text-green-700 font-bold">E-Mail</dt>
+            <dd>bergersvenoliver@gmail.com</dd>
+        </dl>
+    </x-layout.content>
+    <x-layout.content>
+        <x-layout.h2 title="Weitere Informationen"></x-layout.h2>
+        <div class="flex flex-col gap-2">
+            <p>
+            <p>Dies ist ein nicht-kommerzielles, privates Projekt.</p>
+            <p>Die Inhalte dieser Website sind privater Natur und stellen keine journalistisch-redaktionellen Angebote
+                dar.</p><br>
+            <p>Auf dieser Website stelle ich eigene Projekte vor, betreibe einen Blog und eine Datenbank mit selbst
+                erlerntem Wissen.</p>
+            <p>Es erfolgt <span class="text-bold">keine</span> Gewinnerzielungsabsicht mit <a class="text-normal"
+                    href="/">dieser
+                    Projektseite</a>.</p>
+            </p>
+        </div>
     </x-layout.content>
 </x-public-layout>
--- resources/views/privacy-policy.blade.php
+++ resources/views/privacy-policy.blade.php
@@ -1,14 +1,202 @@
 <x-public-layout>
+    <div class="privacy-policy">
+        <x-layout.content>
+            <p>Der Schutz Ihrer personenbezogenen Daten ist ein wichtiges Anliegen.</p>
+            <p>Diese Website dient der Präsentation privater Projekte sowie eines Blogs bzw. einer Wissensdatenbank.</p>
+            <p>Die Verarbeitung personenbezogener Daten erfolgt ausschließlich in dem Umfang, der für den Betrieb der
+                Website, die Bereitstellung der Funktionen und die Kommunikation erforderlich ist.</p>
+        </x-layout.content>
+        <x-layout.content>
+            <x-layout.h2 title="Verantwortliche Stelle"></x-layout.h2>
 
-    <x-layout.content>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur reprehenderit unde incidunt voluptatem
-            reiciendis odio voluptas delectus ipsam ea, accusamus, ipsa quam officiis corrupti rem consequatur ad
-            voluptatibus sint dolorem eos? Nulla adipisci quidem aspernatur dolores velit, facere hic! Commodi maxime
-            sunt,
-            earum, nam ex illum, id quis suscipit natus vitae quod dignissimos tempore? Tenetur a porro velit temporibus
-            provident magnam accusamus minus sapiente corporis quibusdam dolor, natus culpa maxime, aliquid similique
-            harum
-            quidem amet reiciendis vel eum. Natus modi voluptatum alias, sit in, animi eum distinctio eveniet veritatis,
-            fugiat delectus laborum nisi perspiciatis voluptatibus nobis consequuntur. Cumque, obcaecati facilis!</p>
-    </x-layout.content>
+            <p>
+                Sven Oliver Berger<br>
+                Limburger Straße 30<br>
+                65510 Idstein<br><br>
+                Telefon: +49 1511 9409788<br>
+                E-Mail: <a href="mailto:bergersvenoliver@gmail.com">bergersvenoliver@gmail.com</a>
+            </p>
+        </x-layout.content>
+
+
+        <x-layout.content>
+            <x-layout.h2 title="Verwendete Skripte und Bibliotheken"></x-layout.h2>
+            <p>Auf dieser Website werden ausschließlich <span class="font-bold">lokal eingebundene</span> Bibliotheken
+                verwendet.</p>
+            <p>Ein Datenaustausch mit externen Drittanbietern zu Analyse-, Tracking- oder Marketingzwecken findet
+                nicht statt.</p>
+            <table class="table-auto text-left mt-20 w-full">
+                <thead>
+                    <tr>
+                        <th class="pb-10">Status</th>
+                        <th class="pb-10">Technologie</th>
+                        <th class="pb-10">Version</th>
+                        <th class="pb-10">Verwendungszweck</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="pb-4">
+                            <span class="label success">Umgesetzt</span>
+                        </td>
+                        <td class="pb-4">
+                            <a href="https://laravel.com/">Laravel</a> (+ <a href="https://vitejs.dev/">vite</a>)
+                        </td>
+                        <td class="pb-4">
+                            <span>12.52.0 (+ 7.3.1)</span>
+                        </td>
+                        <td class="pb-4">
+                            <span class="font-bold">Backend-Framework</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="pb-4">
+                            <span class="label success">Umgesetzt</span>
+                        </td>
+                        <td class="pb-4">
+                            <a href="https://tailwindcss.com/">Tailwind CSS</a>
+                        </td>
+                        <td class="pb-4">
+                            <span>4.2</span>
+                        </td>
+                        <td class="pb-4">
+                            <span class="font-bold">CSS-Framework</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="pb-4">
+                            <span class="label planning">In Planung</span>
+                        </td>
+                        <td class="pb-4">
+                            <a href="https://react.dev">ReactJS</a>
+                        </td>
+                        <td class="pb-4">
+                            <span>19.2</span>
+                        </td>
+                        <td class="pb-4">
+                            <span class="font-bold">Frontend-Bibliothek</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="pb-4">
+                            <span class="label success">Umgesetzt</span>
+                        </td>
+                        <td class="pb-4">
+                            <a href="https://ckeditor.com/">CKEditor</a>
+                        </td>
+                        <td class="pb-4">
+                            <span>5.0</span>
+                        </td>
+                        <td class="pb-4">
+                            <strong>WYSIWYG-Editor</strong>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="pb-4">
+                            <span class="label success">Umgesetzt</span>
+                        </td>
+                        <td class="pb-4">
+                            <a href="https://fontawesome.com/">Font Awesome</a>
+                        </td>
+                        <td class="pb-4">
+                            <span>7.2.0</span>
+                        </td>
+                        <td class="pb-4">
+                            <strong>Icons</strong>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="pb-4">
+                            <span class="label">In Kürze</span>
+                        </td>
+                        <td class="pb-4">
+                            <a href="https://highlightjs.org/">Highlight.js</a>
+                        </td>
+                        <td class="pb-4">
+                            <span>11.11.1</span>
+                        </td>
+                        <td class="pb-4">
+                            <span class="font-bold">Code-Syntax-Highlighting</span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </x-layout.content>
+
+        <x-layout.content>
+            <x-layout.h2 title="Einbindung externer Schnittstellen (APIs)"></x-layout.h2>
+            <p>Zur Darstellung bestimmter Inhalte oder Funktionen können technische Schnittstellen (APIs) externer
+                Drittanbieter eingebunden sein.<br>Dies betrifft insbesondere die dynamische Bereitstellung von
+                Medien-, Informations- oder sonstigen Inhaltsdaten.</p>
+            <p>Beim Aufruf entsprechender Inhalte stellt Ihr Browser eine direkte Verbindung zu den
+                Servern des jeweiligen Drittanbieters her.</p><br>
+            <p>Dabei werden technisch notwendige Verbindungsdaten übermittelt, insbesondere:</p>
+            <ol>
+                <li>IP-Adresse</li>
+                <li>Datum und Uhrzeit des Zugriffs</li>
+                <li>Browsertyp und Betriebssystem</li>
+                <li>gegebenenfalls die aufgerufene Seite (Referrer)</li>
+            </ol>
+            <p>Die Übermittlung dieser Daten erfolgt ausschließlich zur technischen Bereitstellung der angeforderten
+                Inhalte.<br>Der Betreiber dieser Website hat keinen Einfluss auf die konkrete Datenverarbeitung
+                durch den jeweiligen Drittanbieter.<br>Es gelten insoweit die Datenschutzbestimmungen des jeweiligen
+                Anbieters.</p>
+            <p>Rechtsgrundlage für die Einbindung externer APIs ist Art. 6 Abs. 1 lit. f DSGVO (berechtigtes
+                Interesse an der funktionalen Darstellung der Website).</p>
+        </x-layout.content>
+
+        <x-layout.content>
+            <x-layout.h2 title="Login über Google und GitHub"></x-layout.h2>
+            <p>Auf dieser Website können Sie sich optional über Google oder GitHub anmelden (OAuth-Verfahren).</p>
+            <p>Bei Nutzung dieser Funktion wird eine Verbindung zum jeweiligen Anbieter hergestellt. Dabei können
+                insbesondere IP-Adresse sowie Datum/Uhrzeit des Zugriffs an den Anbieter übermittelt werden.</p>
+            <p>Im Rahmen der Anmeldung werden vom Anbieter bereitgestellte Profildaten verarbeitet, insbesondere:
+                Nutzer-ID des Anbieters, Name und E-Mail-Adresse (soweit verfügbar).</p>
+            <p>Diese Daten werden ausschließlich zur Authentifizierung, Zuordnung bzw. Erstellung eines Benutzerkontos
+                und zur Bereitstellung des Login-Systems verarbeitet.</p>
+            <p>Rechtsgrundlage ist Art. 6 Abs. 1 lit. b DSGVO (Durchführung vorvertraglicher Maßnahmen /
+                Vertragsdurchführung) sowie Art. 6 Abs. 1 lit. f DSGVO (berechtigtes Interesse an einem sicheren und
+                nutzerfreundlichen Login-Verfahren).</p>
+            <p>Es gelten ergänzend die Datenschutzbestimmungen der jeweiligen Anbieter:</p>
+            <ul>
+                <li><a href="https://policies.google.com/privacy" target="_blank" rel="noopener noreferrer">Google
+                        Datenschutzerklärung</a></li>
+                <li><a href="https://docs.github.com/de/site-policy/privacy-policies/github-general-privacy-statement"
+                        target="_blank" rel="noopener noreferrer">GitHub Datenschutzerklärung</a></li>
+            </ul>
+        </x-layout.content>
+        <x-layout.content>
+            <x-layout.h2 title="Cookies und Tracking"></x-layout.h2>
+            <p>Diese Website verwendet technisch notwendige Cookies für den Betrieb der Anwendung, insbesondere
+                Session-Cookies zur Anmeldung sowie Sicherheitsmechanismen (z.B. CSRF-Schutz).</p>
+            <p>Diese Cookies sind für den sicheren Betrieb der Website erforderlich und dienen nicht zu Marketing-
+                oder Trackingzwecken.</p>
+            <p>Es kommt kein Tracking- oder Analysedienst (z.B. Google Analytics) zum Einsatz.</p>
+        </x-layout.content>
+
+        <x-layout.content>
+            <x-layout.h2 title="Kontaktaufnahme"></x-layout.h2>
+            <p>Wenn Sie per E-Mail Kontakt aufnehmen, werden die übermittelten Daten ausschließlich zur Bearbeitung
+                Ihrer Anfrage verwendet.</p>
+            <p>Eine Weitergabe an Dritte erfolgt nicht, sofern keine gesetzliche Verpflichtung besteht.</p>
+        </x-layout.content>
+        <x-layout.content>
+            <x-layout.h2 title="Ihre Rechte"></x-layout.h2>
+            <p>Sie haben das Recht auf Auskunft über die zu Ihrer Person gespeicherten Daten sowie auf Berichtigung,
+                Löschung oder Einschränkung der Verarbeitung.</p>
+            <p>Hierzu können Sie sich jederzeit an die oben genannte verantwortliche Stelle wenden.</p>
+            <p>Unabhängig davon können Sie Ihr Benutzerkonto selbst löschen. Dabei werden Ihre personenbezogenen Daten
+                gelöscht, soweit keine gesetzlichen
+                Aufbewahrungspflichten bestehen.</p>
+            <p>Darüber hinaus haben Sie das Recht auf Datenübertragbarkeit sowie das Recht, Widerspruch gegen die
+                Verarbeitung einzulegen, soweit die gesetzlichen Voraussetzungen vorliegen.</p>
+        </x-layout.content>
+        <x-layout.content>
+            <x-layout.h2 title="Änderungen der Datenschutzerklärung"></x-layout.h2>
+            <p>Diese Datenschutzerklärung kann von Zeit zu Zeit aktualisiert werden.</p>
+            <p>Es wird empfohlen, die Erklärung regelmäßig zu überprüfen, um über Änderungen informiert zu bleiben.</p>
+            <p>Die aktuelle Version der Datenschutzerklärung ist jederzeit auf dieser Seite verfügbar.</p>
+            <p class="mt-10 font-bold italic">Stand: 22.02.2026</p>
+        </x-layout.content>
+    </div>
 </x-public-layout>
