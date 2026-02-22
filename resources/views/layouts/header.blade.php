fd4d0d6cee50f7022e7c8c0ec6a4de3af0322d6d
Sven Oliver Berger <bergersvenoliver@gmail.com>
2026-02-22 16:34:29 +0100

    Mäh...

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,6 +1,6 @@
 <header class="bg-red-800 rounded-2xl flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between p-4 sm:p-6">
     <h2 class="text-2xl sm:text-3xl text-white">
-        <a href="{{ route('index') }}">RiftCore.de</a>
+        <a href="{{ route('index') }}">RiftCore.de (Laravel + Tailwind + Stimulus)</a>
     </h2>
 
     <nav class="flex flex-wrap items-center gap-4 text-white text-base sm:text-lg">
