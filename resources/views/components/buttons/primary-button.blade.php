462a7a7b25015b0737b5bac66f3b3093549f5d05
Sven Oliver Berger <bergersvenoliver@gmail.com>
2026-02-22 13:29:08 +0100

    change colors

--- resources/views/components/buttons/primary-button.blade.php
+++ resources/views/components/buttons/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center justify-center bg-blue-500 text-white rounded-2xl px-6 py-4 text-center hover:bg-blue-600 transition w-full sm:w-auto']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center justify-center bg-green-600 text-white rounded-2xl px-6 py-4 text-center hover:bg-green-700 transition w-full sm:w-auto']) }}>
     {{ $slot }}
 </button>
--- resources/views/components/buttons/secondary-button.blade.php
+++ resources/views/components/buttons/secondary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'button', 'class' => 'buttonSecondary bg-yellow-600 inline-flex items-center text-white justify-center border border-gray-200 rounded-2xl px-6 py-4 text-center hover:bg-yellow-700 transition w-full sm:w-auto disabled:opacity-50']) }}>
+<button {{ $attributes->merge(['type' => 'button', 'class' => 'buttonSecondary bg-yellow-500 inline-flex items-center text-white justify-center border border-gray-200 rounded-2xl px-6 py-4 text-center hover:bg-yellow-600 transition w-full sm:w-auto disabled:opacity-50']) }}>
     {{ $slot }}
 </button>
